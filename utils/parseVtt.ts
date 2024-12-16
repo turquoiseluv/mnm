992b45f23d84273646dae8daa5c877445395d25e
jskim <turquoiseluv@naver.com>
2024-12-17 00:52:12 +0900

    1.2.0
    update vtt parse function

--- utils/parseVtt.ts
+++ utils/parseVtt.ts
@@ -5,8 +5,13 @@ interface VttCue {
 }
 
 export const parseVttFile = (vttText: string): VttCue[] => {
+	//cleanse text
+	vttText = vttText.replace(/\r\n/g, '\n').replace(/\r/g, '\n');
+	vttText = vttText.replace(/[\u200B-\u200D\uFEFF]/g, '');
+
 	const cues: VttCue[] = [];
-	const cueRegex = /(\d+:\d{2}:\d{2}.\d{3}) --> (\d+:\d{2}:\d{2}.\d{3})\n([^\n]+)/g;
+	// const cueRegex = /(\d+:\d{2}:\d{2}.\d{3}) --> (\d+:\d{2}:\d{2}.\d{3})\n([^\n]+)/g;
+	const cueRegex = /(\d+:\d{2}:\d{2}.\d{3}) --> (\d+:\d{2}:\d{2}.\d{3})\n([\s\S]*?)(?:\n{2,}|$)/g;
 	let match: RegExpExecArray | null;
 
 	// Loop through each VTT cue and extract data
