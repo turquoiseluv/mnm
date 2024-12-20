d9632904c9d7db19da0f97f8a8f086d2637ed29f
turquoiseluv <turquoiseluv@naver.com>
2024-12-20 13:18:22 +0900

    1.2.1

--- app/vtt/page.tsx
+++ app/vtt/page.tsx
@@ -1,27 +1,39 @@
 "use client";
 
+import { parseVttFile } from "@/utils/parseVtt";
 import { Button } from "@mantine/core";
 import { motion } from "motion/react";
 import { useEffect, useState } from "react";
 
-type dataType = {
-	id: number;
-	title: string;
-};
-
 export default function Vtt() {
 	const [vttIdx, setVttIdx] = useState(0);
-	const [productsData, setProductsData] = useState([]);
+	const [productsData, setProductsData] = useState<VTTCue[]>([]);
 	const [showProductOptions, setShowProductOptions] = useState(false);
 
-	useEffect(() => {
-		async function fetchData() {
-			const res = await fetch("https://dummyjson.com/products");
-			const data = await res.json();
-			setProductsData(data.products);
-		}
+	// useEffect(() => {
+	// 	async function fetchData() {
+	// 		const res = await fetch("https://dummyjson.com/products");
+	// 		const data = await res.json();
+	// 		setProductsData(data.products);
+	// 	}
 
-		fetchData();
+	// 	fetchData();
+	// }, []);
+
+	useEffect(() => {
+		const fetchVtt = async () => {
+			try {
+				const response = await fetch("sample/sample.vtt");
+				const text = await response.text();
+				const parsedCaptions = parseVttFile(text);
+				
+				setProductsData(parsedCaptions);
+			} catch (error) {
+				console.error("Error fetching VTT file:", error);
+			}
+		};
+
+		fetchVtt();
 	}, []);
 
 	// useEffect(() => {
@@ -56,23 +68,25 @@ export default function Vtt() {
 			</div>
 
 			<div className="justify-center items-center bg-black text-white flex h-[20rem]">
-				{productsData.map((data: dataType) => {
+				{productsData.map((data: VTTCue, idx:number) => {
+					idx++;
 					return (
 						<motion.div
-							key={data.id}
-							className={`flex absolute`}
-							initial={{ fontSize: "1.5rem", opacity: 0 }}
+							key={idx}
+							className={`flex absolute p-10`}
+							initial={{ fontSize: "1.5em", opacity: 0, userSelect: "none" }}
 							animate={{
-								color: Math.abs(vttIdx - data.id) > 0 ? "rgba(255,255,255,0)" : "rgba(255,255,255,1)",
-								textShadow: Math.abs(vttIdx - data.id) === 1 ? "0 0 5px white" : "0 0 -1px white",
-								opacity: vttIdx - data.id <= 1 ? 1 : 0,
+								color: Math.abs(vttIdx - idx) > 0 ? "rgba(255,255,255,0)" : "rgba(255,255,255,1)",
+								textShadow: Math.abs(vttIdx - idx) === 1 ? "0 0 8px white" : "0 0 -1px white",
+								opacity: vttIdx - idx === 0 ? 1 : Math.abs(vttIdx - idx) === 1 ? 0.5 : 0,
 								transition: { duration: 0.2 },
-								y: vttIdx < data.id ? "3rem" : vttIdx > data.id ? "-3rem" : "0",
-								scaleX: vttIdx - data.id === 0 ? 1.5 : Math.abs(vttIdx - data.id) === 1 ? 1 : 0,
-								scaleY: vttIdx - data.id === 0 ? 1.5 : Math.abs(vttIdx - data.id) === 1 ? 1 : 0,
+								y: vttIdx < idx ? "1em" : vttIdx > idx ? "-1em" : "0",
+								z: vttIdx - idx === 0 ? 10 : 1,
+								scaleX: vttIdx - idx === 0 ? 1 : Math.abs(vttIdx - idx) === 1 ? 0.8 : 0,
+								scaleY: vttIdx - idx === 0 ? 1 : Math.abs(vttIdx - idx) === 1 ? 0.6 : 0,
 							}}
 						>
-							<div>{data.title}</div>
+							<div>{data.text}</div>
 						</motion.div>
 					);
 				})}
--- utils/parseVtt.ts
+++ utils/parseVtt.ts
@@ -1,15 +1,22 @@
-interface VttCue {
-	startTime: string;
-	endTime: string;
-	text: string;
-}
+// interface VttCue {
+// 	startTime: string;
+// 	endTime: string;
+// 	text: string;
+// }
 
-export const parseVttFile = (vttText: string): VttCue[] => {
+const parseVttTimeStr = (timeStr: string) => {
+	let time = 0;
+	const [h, m, s] = timeStr.split(":");
+	time = Number(h) * 60 * 60 + Number(m) * 60 + Number(s);
+	return time;
+};
+
+export const parseVttFile = (vttText: string): VTTCue[] => {
 	//cleanse text
-	vttText = vttText.replace(/\r\n/g, '\n').replace(/\r/g, '\n');
-	vttText = vttText.replace(/[\u200B-\u200D\uFEFF]/g, '');
+	vttText = vttText.replace(/\r\n/g, "\n").replace(/\r/g, "\n");
+	vttText = vttText.replace(/[\u200B-\u200D\uFEFF]/g, "");
 
-	const cues: VttCue[] = [];
+	const cues: VTTCue[] = [];
 	// const cueRegex = /(\d+:\d{2}:\d{2}.\d{3}) --> (\d+:\d{2}:\d{2}.\d{3})\n([^\n]+)/g;
 	const cueRegex = /(\d+:\d{2}:\d{2}.\d{3}) --> (\d+:\d{2}:\d{2}.\d{3})\n([\s\S]*?)(?:\n{2,}|$)/g;
 	let match: RegExpExecArray | null;
@@ -17,7 +24,7 @@ export const parseVttFile = (vttText: string): VttCue[] => {
 	// Loop through each VTT cue and extract data
 	while ((match = cueRegex.exec(vttText)) !== null) {
 		const [_, startTime, endTime, text] = match;
-		cues.push({ startTime, endTime, text });
+		cues.push(new VTTCue(parseVttTimeStr(startTime), parseVttTimeStr(endTime), text));
 	}
 
 	return cues;
